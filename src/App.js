9d03f0d5e623b35e45b685d665d964c521c372a3
Hamdan-Vohra <hamdanvohra5676@gmail.com>
2024-07-16 00:45:48 +0500

    Added Routes and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 
+//importing all components
+import { Header, Nav, Footer, Home, About, PostPage, NewPost, Missing } from './Exporter'
+import { Route, Routes, BrowserRouter as Router, Switch, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/post" element={<PostPage />} />
+        <Route path='/post/:id' element={<NewPost />} />
+        <Route path="/about" element={<About />} />
+        <Route path='*' element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Exporter.js
@@ -0,0 +1,10 @@
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import About from './components/About'
+import PostPage from './components/PostPage'
+import NewPost from './components/NewPost'
+import Missing from './components/Missing'
+
+export { Header, Nav, Footer, Home, About, PostPage, NewPost, Missing }
